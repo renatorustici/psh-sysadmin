52509f310d24b5af77c14c227a9b924262339415
Renato Rustici <renato.rustici@gmail.com>
2020-11-28 15:50:26 -0300

    Adding, Migrate-ImmutableID

---
+++ Migrate-ImmutableID.ps1
@@ -0,0 +1,53 @@
+param($Path)
+
+if ($null -eq $Path)
+{
+    Write-Host "Please, provide the file containing both, the AD users and the 365 UPN."
+    Write-Host "Format: samAccountName,o365Upn"
+
+    exit
+}
+
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+
+Write-Host "The path is $($Path)`n"
+
+$users = Get-Content -Path $Path
+$failed = New-Object -TypeName "System.Collections.ArrayList"
+
+foreach($user in $users)
+{
+    $field = $user.split(",")
+
+    $samAccountName = $field[0]
+    $o365Upn = $field[1]
+
+    Write-Host "   >> Configuring $($samAccountName) ..."
+
+    try
+    {
+        $imuid = [system.convert]::ToBase64String((Get-ADUser $samAccountName).objectGUid.ToByteArray())
+        Set-MsolUser -UserPrincipalName $o365Upn `
+                     -ImmutableId $imuid
+    }
+    catch
+    {
+        Write-Warning $error[0]
+        $failed += $samAccountName
+    }
+}
+
+if($failed.Count -gt 0)
+{
+    Write-Host "`nThe following users were NOT update successfully:"
+
+    foreach($user in $failed)
+    {
+        Write-Host -ForegroundColor red $user
+    }
+}
+else
+{
+    Write-Host "`nAll cloud users were updated successfully."
+}
+
